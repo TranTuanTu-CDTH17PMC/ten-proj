c950c6acfaecd3acf6666a026708d068d7ff1b35
NguyenPhucHoaThanh-CDTH17PMC <meoluoi8a3.tn@gmail.com>
2019-11-10 01:49:00 -0800

    cập nhật model Admin

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Admin extends Model
 {
     protected $table = 'admin';
+     use SoftDeletes;
+ 
+  protected $dates = ['deleted_at'];
 }
